0e7a8f743fbda50a56d1b1eaf34a5afbaf133631
omizha <ada.lovelace@kakao.com>
2023-10-04 09:10:26 +0900

    feat: 유저모집 좌패널 개선

--- app/client-react/src/page/@backoffice/component/LeftPanel.tsx
+++ app/client-react/src/page/@backoffice/component/LeftPanel.tsx
@@ -22,9 +22,22 @@ const LeftPanel = () => {
 
   return (
     <Container>
-      <QRCodeSVG width="340px" height="340px" value="http://127.0.0.1:5173" />
-      <>{userList.length}명 참가 완료</>
-      <button onClick={onStartGame}>게임 시작</button>
+      <QRCodeContainer>
+        <QRCodeTextWrapper>스마트폰 카메라로 스캔</QRCodeTextWrapper>
+        <QRCodeWrapper>
+          <QRCodeSVG width="340px" height="340px" value="http://127.0.0.1:5173" />
+        </QRCodeWrapper>
+      </QRCodeContainer>
+      <UserListWrapper>
+        {userList.map((user) => (
+          <ProfileContainer key={user.nickname}>
+            <Avatar src={user.profilePictureUrl} alt={`${user.nickname}님의 프로필 사진`} />
+            <div>{user.nickname}</div>
+          </ProfileContainer>
+        ))}
+      </UserListWrapper>
+      {/* <>{userList.length}명 참가 완료</> */}
+      {/* <button onClick={onStartGame}>게임 시작</button> */}
     </Container>
   );
 };
@@ -39,6 +52,71 @@ const Container = styled.div`
   background-color: #dddddd;
   padding: 10px;
   gap: 20px;
+`;
+
+const QRCodeContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+`;
+
+const QRCodeTextWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  background: #777777;
+  border-radius: 0.4em;
+  width: 340px;
+  height: 80px;
+  font-size: 32px;
+  color: #ffffff;
+
+  &:after {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    width: 0;
+    height: 0;
+    border: 20px solid transparent;
+    border-top-color: #777777;
+    border-bottom: 0;
+    margin-left: -20px;
+    margin-bottom: -20px;
+  }
+`;
+
+const QRCodeWrapper = styled.div`
+  width: 340px;
+  height: 340px;
+  background-color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  box-sizing: border-box;
+  padding: 20px;
+  border: 10px solid #777777;
+`;
+
+const ProfileContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Avatar = styled.img`
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  object-fit: cover;
+`;
+
+const UserListWrapper = styled.div`
+  display: flex;
+  flex: 0.7;
+  width: 340px;
+  background-color: #ddeedd;
 `;
 
 export default LeftPanel;
