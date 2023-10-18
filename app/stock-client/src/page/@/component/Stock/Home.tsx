511b90abb12206c2fad576e477c9bfa373199ab3
omizha <ada.lovelace@kakao.com>
2023-10-18 13:05:53 +0900

    feat: 주식게임 홈 리팩토링

--- app/stock-client/src/page/@/component/Stock/Home.tsx
+++ app/stock-client/src/page/@/component/Stock/Home.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import { useAtomValue } from 'jotai';
 import { commaizeNumber } from '@toss/utils';
 import { getDateDistance } from '@toss/date';
@@ -21,22 +20,13 @@ const Home = () => {
 
   return (
     <>
-      <Container>
-        <ContainerTitle>잔액</ContainerTitle>
-        <ContainerBolder>{commaizeNumber(user.money)}원</ContainerBolder>
-      </Container>
-      <Container>
-        <ContainerTitle>주식 가치</ContainerTitle>
-        <ContainerBolder>{commaizeNumber(allSellPrice)}원</ContainerBolder>
-      </Container>
-      <Container>
-        <ContainerTitle>모두 팔고 난 뒤의 금액</ContainerTitle>
-        <ContainerBolder>{commaizeNumber(user.money + allSellPrice)}원</ContainerBolder>
-      </Container>
-      <Container>
-        <ContainerTitle>모두 팔고 난 뒤의 순이익</ContainerTitle>
-        <ContainerBolder>{(((user.money + allSellPrice) / 1000000) * 100 - 100).toFixed(2)}%</ContainerBolder>
-      </Container>
+      <Box title="잔액" value={`${commaizeNumber(user.money)}원`} />
+      <Box title="주식 가치" value={`${commaizeNumber(allSellPrice)}원`} />
+      <Box title="모두 팔고 난 뒤의 금액" value={`${commaizeNumber(user.money + allSellPrice)}원`} />
+      <Box
+        title="모두 팔고 난 뒤의 순이익"
+        value={`${(((user.money + allSellPrice) / 1000000) * 100 - 100).toFixed(2)}%`}
+      />
       <Box
         title="진행 시간"
         value={`${prependZero(getDateDistance(game.startedTime, new Date()).minutes, 2)}:${prependZero(
@@ -47,33 +37,5 @@ const Home = () => {
     </>
   );
 };
-
-const H2 = styled.h2``;
-
-const H4 = styled.h4``;
-
-const Container = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  background-color: #ffffff;
-  box-sizing: border-box;
-  padding: 12px;
-  border-radius: 8px;
-  overflow: hidden;
-
-  margin-bottom: 12px;
-`;
-
-const ContainerTitle = styled.div`
-  font-size: 12px;
-  font-weight: 500;
-  margin-bottom: 8px;
-`;
-
-const ContainerBolder = styled.div`
-  font-size: 18px;
-  font-weight: bolder;
-`;
 
 export default Home;
