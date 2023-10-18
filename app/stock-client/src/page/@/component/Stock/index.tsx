98cff9e9fad41a3facd8b0f770f0c5462256e72b
하정훈 <jhha@miridih.com>
2023-10-18 13:01:26 +0900

    feat: 주식게임 새로운 네비게이션 헤더 적용

--- app/stock-client/src/page/@/component/Stock/index.tsx
+++ app/stock-client/src/page/@/component/Stock/index.tsx
@@ -1,20 +1,77 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import styled from '@emotion/styled';
 import { useSearchParams } from 'react-router-dom';
 import { SwitchCase } from '@toss/react';
-import BottomNav from './BottomNav';
-import Header from '../Header';
+import { Tabs, TabsProps } from 'antd';
+import { css } from '@emotion/react';
 import Home from './Home';
 import Buy from './Buy';
 import Sell from './Sell';
 
+const navItem = ['홈', '사기', '팔기'] as const;
+type NavName = (typeof navItem)[number];
+
+const items: TabsProps['items'] = [
+  {
+    children: <></>,
+    key: '홈',
+    label: '홈',
+  },
+  {
+    children: <></>,
+    key: '사기',
+    label: '사기',
+  },
+  {
+    children: <></>,
+    key: '팔기',
+    label: '팔기',
+  },
+];
+
 const Stock = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+
+  const onClickTab = useCallback(
+    (key: string) => {
+      switch (key) {
+        case '홈':
+          setSearchParams({ page: '홈' });
+          break;
+        case '사기':
+          setSearchParams({ page: '사기' });
+          break;
+        case '팔기':
+          setSearchParams({ page: '팔기' });
+          break;
+        default:
+          setSearchParams({ page: '홈' });
+          break;
+      }
+    },
+    [setSearchParams],
+  );
 
   return (
     <>
       <Container>
-        <Header title={searchParams.get('page') ?? ''} />
+        {/* <Header title={searchParams.get('page') ?? ''} /> */}
+        <div
+          css={css`
+            padding: 0 16px;
+            background-color: #ffffff;
+          `}
+        >
+          <Tabs
+            size="large"
+            defaultActiveKey={searchParams.get('page') ?? '홈'}
+            items={items}
+            onChange={onClickTab}
+            tabBarStyle={{
+              margin: '0 0 0 0',
+            }}
+          />
+        </div>
         <ContentContainer>
           <SwitchCase
             value={searchParams.get('page') ?? '홈'}
@@ -27,7 +84,7 @@ const Stock = () => {
           />
         </ContentContainer>
       </Container>
-      <BottomNav />
+      {/* <BottomNav /> */}
     </>
   );
 };
