e8de387ff34cffae2bee09afdf6566957a661f1c
omizha <ada.lovelace@kakao.com>
2023-10-19 02:44:47 +0900

    feat: 에러핸들링

--- app/stock-client/src/hook/query/game/useBuyStock.tsx
+++ app/stock-client/src/hook/query/game/useBuyStock.tsx
@@ -2,19 +2,27 @@ import { useMutation } from '@tanstack/react-query';
 import { Response } from 'shared~type';
 import { Request } from 'shared~type-stock';
 import { serverApiUrl } from '../../../config/baseUrl';
+import handleResponse from '../../../service/handleResponse';
 
 const useBuyStock = () => {
-  const { mutateAsync } = useMutation<Response.Game, unknown, Request.BuyStock>(['useBuyStock'], async (data) => {
-    const response = await fetch(`${serverApiUrl}/game/stock/buy`, {
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    });
+  const { mutateAsync, error } = useMutation<Response.Game, unknown, Request.BuyStock>(
+    ['useBuyStock'],
+    async (data) => {
+      const response = await fetch(`${serverApiUrl}/game/stock/buy`, {
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
 
-    return response.json();
-  });
+      return handleResponse(response);
+    },
+    {
+      useErrorBoundary: false,
+    },
+  );
+  console.debug('🚀 ~ file: useBuyStock.tsx:9 ~ useBuyStock ~ error:', error);
 
   return { mutateAsync };
 };
--- app/stock-client/src/hook/query/game/useSellStock.tsx
+++ app/stock-client/src/hook/query/game/useSellStock.tsx
@@ -2,19 +2,26 @@ import { useMutation } from '@tanstack/react-query';
 import { Response } from 'shared~type';
 import { Request } from 'shared~type-stock';
 import { serverApiUrl } from '../../../config/baseUrl';
+import handleResponse from '../../../service/handleResponse';
 
 const useSellStock = () => {
-  const { mutateAsync } = useMutation<Response.Game, unknown, Request.SellStock>(['useSellStock'], async (data) => {
-    const response = await fetch(`${serverApiUrl}/game/stock/sell`, {
-      body: JSON.stringify(data),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      method: 'POST',
-    });
+  const { mutateAsync } = useMutation<Response.Game, unknown, Request.SellStock>(
+    ['useSellStock'],
+    async (data) => {
+      const response = await fetch(`${serverApiUrl}/game/stock/sell`, {
+        body: JSON.stringify(data),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        method: 'POST',
+      });
 
-    return response.json();
-  });
+      return handleResponse(response);
+    },
+    {
+      useErrorBoundary: false,
+    },
+  );
 
   return { mutateAsync };
 };
