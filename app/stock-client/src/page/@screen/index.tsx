b19fc17ddc8afdc328e67d46b2b309c46c81fef9
omizha <ada.lovelace@kakao.com>
2023-10-27 02:01:10 +0900

    feat: 주식게임 스크린 QR코드 표시

--- app/stock-client/src/page/@screen/index.tsx
+++ app/stock-client/src/page/@screen/index.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
 import React from 'react';
 import { getDateDistance } from '@toss/date';
+import { SwitchCase } from '@toss/react';
+import { QRCode } from 'antd';
 import Table from './component/Table';
 import { Query } from '../../hook';
 import prependZero from '../../service/prependZero';
@@ -12,19 +14,39 @@ export default function Screen() {
   const { data: game } = Query.Game.useGame();
 
   const startedTime = game?.startedTime ?? new Date();
+  const gamePhase = game?.gamePhase ?? 'CROWDING';
 
   const { seconds, minutes } = getDateDistance(startedTime, new Date());
 
   return (
     <>
-      <TimeBox>
-        {prependZero(minutes, 2)}:{prependZero(seconds, 2)}
-      </TimeBox>
-      <Wrapper>
-        <Container>
-          <Table />
-        </Container>
-      </Wrapper>
+      <SwitchCase
+        value={gamePhase}
+        caseBy={{
+          PLAYING: (
+            <>
+              <TimeBox>
+                {prependZero(minutes, 2)}:{prependZero(seconds, 2)}
+              </TimeBox>
+              <Wrapper>
+                <Container>
+                  <Table />
+                </Container>
+              </Wrapper>
+            </>
+          ),
+        }}
+        defaultComponent={
+          <>
+            <TimeBox>QR코드를 스캔하여 입장하세요</TimeBox>
+            <Wrapper>
+              <Container>
+                <QRCode value={window.location.origin} bgColor="#ffffff" size={300} />
+              </Container>
+            </Wrapper>
+          </>
+        }
+      />
     </>
   );
 }
