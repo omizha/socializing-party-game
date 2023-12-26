61bc6652fd9fe042fbf13413036cbe4c855f6ca4
omizha <ada.lovelace@kakao.com>
2023-12-26 19:51:21 +0900

    feat: Add useMutation hook to lib-react-query

--- package/library/lib-react-query/src/index.ts
+++ package/library/lib-react-query/src/index.ts
@@ -1,2 +1,3 @@
 export { default as QueryClientProvider } from './QueryClientProvider';
 export { default as useQuery } from './hook/useQuery';
+export { default as useMutation } from './hook/useMutation';
